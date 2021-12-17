f9529698d8fd444ee29049c49a0d91e51e1fb507
Liz Miyuna <neverwayhelp@gmail.com>
2021-12-16 21:40:37 -0500

    Added quit functionality (Kind of)

---
+++ U3D-Feral Strife (Fight Game Concept)/Assets/Resources/Scripts/Main Menu/FS_Menu_Events.cs
@@ -0,0 +1,30 @@
+//=========== Written by Arthur W. Sheldon AKA Lizband_UCC ====================
+//
+// SID: 
+// Purpose: 
+// Applied to: 
+// Editor script: 
+// Notes: 
+//
+//=============================================================================
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class FS_Menu_Events : MonoBehaviour
+{
+    // Public variables
+
+    // Private variables
+
+    // Reference variables
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
